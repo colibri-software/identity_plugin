40b9c67aaf65509f03d26c46e4922b11179940a4
Daniel <daniel.deveau@colibri-software.com>
2013-02-14 13:35:07 -0400

    Set up RSpec framework -- no tests currently.

--- identity-engine/app/controllers/identity_engine/sessions_controller.rb
+++ identity-engine/app/controllers/identity_engine/sessions_controller.rb
@@ -5,22 +5,32 @@ module IdentityEngine
   class SessionsController < ApplicationController
     def new
       session[:identity_return_to] ||= request.referer
+      redirect_to request.referer, :info => 'Already signed in!' if current_user
     end
 
     def create
       auth = request.env["omniauth.auth"]
       user = User.where(:provider => auth["provider"], :auth => auth["uid"]).first || User.create_with_omniauth(auth)
       session[:user_id] = user.id
-      redirect_to session[:identity_return_to], :notice => 'Signed in!'
+      redirect_to session[:identity_return_to], :notice => msg_config.sign_in_msg
     end
 
     def destroy
-      session[:user_id] = nil
-      redirect_to :back, :notice => 'Signed out!'
+      if session[:user_id]
+        session[:user_id] = nil
+        redirect_to :back, :notice => msg_config.sign_out_msg
+      else
+        redirect_to :back, :info => 'Already logged out!'
+      end
     end
 
     def failure
       redirect_to :back, :error => 'Authentication failed, please try again.'
+    end
+
+    private
+    def msg_config
+      @configuration ||= Config.first
     end
   end
 end
