f9dbe450f878a8e2faeecc031e897002664b16a1
Daniel <daniel.deveau@colibri-software.com>
2013-02-14 14:16:49 -0400

    Removed Config model. Now config is directly access inside the engine via the plugin_object.

--- identity-engine/app/controllers/identity_engine/sessions_controller.rb
+++ identity-engine/app/controllers/identity_engine/sessions_controller.rb
@@ -12,25 +12,42 @@ module IdentityEngine
       auth = request.env["omniauth.auth"]
       user = User.where(:provider => auth["provider"], :auth => auth["uid"]).first || User.create_with_omniauth(auth)
       session[:user_id] = user.id
-      redirect_to session[:identity_return_to], :notice => msg_config.sign_in_msg
+      redirect_to session[:identity_return_to], :notice => sign_in_msg
     end
 
     def destroy
       if session[:user_id]
         session[:user_id] = nil
-        redirect_to :back, :notice => msg_config.sign_out_msg
+        redirect_to :back, :notice => sign_out_msg
       else
         redirect_to :back, :info => 'Already logged out!'
       end
     end
 
     def failure
-      redirect_to :back, :error => 'Authentication failed, please try again.'
+      redirect_to :back, :error => error_msg
     end
 
     private
     def msg_config
       @configuration ||= Config.first
+    end
+
+    def sign_in_msg
+      msg_or_default(Engine.plugin_object.config['sign_in_msg'], 'Signed in!')
+    end
+
+    def sign_out_msg
+      msg_or_default(Engine.plugin_object.config['sign_out_msg'], 'Signed out!')
+    end
+
+    def error_msg
+      msg_or_default(Engine.plugin_object.config['error_msg'], 'Authentication failed, please try again!')
+    end
+
+    # return the msg if it's not empty otherwise return the default
+    def msg_or_default(msg, default)
+      msg && !msg.empty? ? msg : default
     end
   end
 end
