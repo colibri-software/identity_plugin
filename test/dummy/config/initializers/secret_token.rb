48381f351c8fe9d360ed9ebed91c11cd1859beab
Daniel <daniel.deveau@colibri-software.com>
2013-02-18 10:03:31 -0400

    Removed dead files.

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '322a459f716d798aca81f21d44a82f83ee964bed02111623c6bcad29d1ffc0c84eabdd8c4b6d20f47205cd8f4921baac33c7ca2b4bdf11dfd50071af202f1b96'
