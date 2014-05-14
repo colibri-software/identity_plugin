a6fe6f3259b98c825eec8d5b6973f87ae0d394e7
Charlie Greene <greeneca@gmail.com>
2014-05-14 13:56:31 -0300

    restrict to cms users

--- app/controllers/identity_plugin/application_controller.rb
+++ app/controllers/identity_plugin/application_controller.rb
@@ -1,5 +1,9 @@
 module IdentityPlugin
   class ApplicationController < ActionController::Base
     helper :all
+
+    def signed_in?
+      @signed_in = locomotive_account_signed_in?
+    end
   end
 end
