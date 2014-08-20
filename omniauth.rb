3be8a85275a7db15a471929f658d0abca8472409
Charlie Greene <greeneca@gmail.com>
2014-08-20 15:16:23 -0300

    remove old file

--- omniauth.rb
+++
@@ -1,7 +0,0 @@
-Engine.config.middleware.use OmniAuth::Builder do
-  provider :identity, model: Identity,
-    on_failed_registration: lambda { |env|
-    IdentitiesController.action(:new).call(env)
-  }
-end
-
